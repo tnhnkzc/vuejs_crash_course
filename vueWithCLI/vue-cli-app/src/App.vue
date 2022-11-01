3acc205163ed002ad5f84868832241de5744780e
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-01 16:07:21 +0100

    Add default tasks, created front end, added delete button

---
+++ vueWithCLI/vue-cli-app/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm("Are you sure ?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 14.30",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Dentist Appointment",
+        day: "May 1st at 14.30",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Meeting",
+        day: "April 1st at 14.30",
+        reminder: true,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
