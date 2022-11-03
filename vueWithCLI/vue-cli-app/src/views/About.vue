be1f6bd249223742b2489fd0f57c7ec24ccf7e6c
tnhnkzc <tunahankuzucuu@gmail.com>
2022-11-03 21:16:35 +0100

    Add router and create views

---
+++ vueWithCLI/vue-cli-app/src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
